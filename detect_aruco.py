0ac692449de8c12e32f68b13ff002bd8a98806ba
andrey1908 <andrey.krishtopik@mail.ru>
2023-03-15 22:13:32 +0300

    detect_aruco.py: detect multiple arucos on the image (sort corner points by aruco id for single image)

--- detect_aruco.py
+++ detect_aruco.py
@@ -31,27 +31,49 @@ def detect_images(images_files, K, D, aruco_size, extract_all_corners,
     marker_corners_all = list()
     for image_file in images_files:
         image = cv2.imread(image_file)
+
         corners, ids, _ = \
             cv2.aruco.detectMarkers(image, aruco_dict, parameters=aruco_params)
-        assert len(corners) == 1
-        rvecs, tvecs, _ = cv2.aruco.estimatePoseSingleMarkers(corners, aruco_size, K, D)
+        corners = np.array(corners)
+        # corners.shape = (n, 1, 4, 2)
+        # ids.shape = (n, 1)
+        ind = np.argsort(ids, axis=0)
+        ids = np.take_along_axis(ids, ind, axis=0)
+        corners = np.take_along_axis(corners, np.expand_dims(ind, axis=(-1, -2)), axis=0)
 
-        marker_pose = np.eye(4)
-        marker_pose[0:3, 0:3] = cv2.Rodrigues(rvecs)[0]
-        marker_pose[0:3, 3] = tvecs[0, 0]
-        corners_in_marker_frame = list()
-        for sx, sy in [(-1, 1), (1, 1), (1, -1), (-1, -1)]:
-            # top left corner first
-            corner_in_marker_frame = np.array(
-                [aruco_size / 2 * sx,
-                 aruco_size / 2 * sy,
-                 0, 1]).reshape(-1, 1)
-            corners_in_marker_frame.append(corner_in_marker_frame)
-            if not extract_all_corners:
-                break
-        corners_in_marker_frame = np.array(corners_in_marker_frame)
-        marker_corners = np.matmul(marker_pose, corners_in_marker_frame)
-        marker_corners = marker_corners[:, 0:3, 0]
+        rvecs, tvecs, _ = cv2.aruco.estimatePoseSingleMarkers(corners, aruco_size, K, D)
+        # rvecs.shape = (n, 1, 3)
+        # tvecs.shape = (n, 1, 3)
+
+        n = corners.shape[0]
+
+        marker_poses = np.tile(np.eye(4), (n, 1, 1))
+        for i in range(n):
+            marker_poses[i, 0:3, 0:3], _ = cv2.Rodrigues(rvecs[i])
+            marker_poses[i, 0:3, 3] = tvecs[i, 0]
+        # marker_poses.shape = (n, 4, 4)
+
+        corners_in_marker_frames = list()
+        for i in range(n):
+            corners_in_single_marker_frame = list()
+            for sx, sy in [(-1, 1), (1, 1), (1, -1), (-1, -1)]:
+                # top left corner first
+                single_corner_in_marker_frame = np.array(
+                    [aruco_size / 2 * sx,
+                     aruco_size / 2 * sy,
+                     0, 1]).reshape(-1, 1)
+                corners_in_single_marker_frame.append(single_corner_in_marker_frame)
+                if not extract_all_corners:
+                    break
+            corners_in_single_marker_frame = np.array(corners_in_single_marker_frame)
+            corners_in_marker_frames.append(corners_in_single_marker_frame)
+        corners_in_marker_frames = np.array(corners_in_marker_frames).swapaxes(0, 1)
+        # corners_in_marker_frames.shape = (1 or 4, n, 4, 1)
+
+        marker_corners = np.matmul(marker_poses, corners_in_marker_frames)
+        marker_corners = marker_corners[:, :, 0:3, 0].reshape(-1, 3)
+        # marker_corners.shape = (n or n * 4, 3)
+
         marker_corners_all.append(marker_corners)
 
         if vis_folder is not None:
@@ -62,6 +84,7 @@ def detect_images(images_files, K, D, aruco_size, extract_all_corners,
             vis_image_file = osp.join(
                 vis_folder, Path(image_file).stem + '_vis.jpg')
             cv2.imwrite(vis_image_file, vis_image)
+
     marker_corners_all = np.vstack(marker_corners_all)
     np.save(out_file, marker_corners_all)
 
