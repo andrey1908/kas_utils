1f9e7366479e31323d94c47d4b7339de0ea46596
andrey1908 <andrey.krishtopik@mail.ru>
2023-03-29 22:24:57 +0300

    detect_aruco.py detect_aruco(): returns rejected corners, if number of detected arucos is zero then returns empty corners and poses

--- detect_aruco.py
+++ detect_aruco.py
@@ -21,69 +21,79 @@ def build_parser():
 
 def detect_aruco(image, K, D, aruco_sizes, extract_all_corners,
         aruco_dict, aruco_params):
-    corners, ids, _ = \
+    corners, ids, rejected = \
         cv2.aruco.detectMarkers(image, aruco_dict, parameters=aruco_params)
     corners = np.array(corners)
+    rejected = np.array(rejected)
     n = corners.shape[0]
+    rejected_n = rejected.shape[0]
     # corners.shape = (n, 1, 4, 2)
     # ids.shape = (n, 1)
+    # rejected.shape = (rejected_n, 1, 4, 2)
 
-    ind = np.argsort(ids, axis=0)
-    ids = np.take_along_axis(ids, ind, axis=0)
-    corners = np.take_along_axis(corners, np.expand_dims(ind, axis=(-1, -2)), axis=0)
+    if n != 0:
+        ind = np.argsort(ids, axis=0)
+        ids = np.take_along_axis(ids, ind, axis=0)
+        corners = np.take_along_axis(corners, np.expand_dims(ind, axis=(-1, -2)), axis=0)
 
-    if isinstance(aruco_sizes, (list, tuple)):
-        aruco_sizes = np.array(aruco_sizes)
-    elif not isinstance(aruco_sizes, np.ndarray):
-        aruco_sizes = np.array([aruco_sizes] * n)
-    if len(aruco_sizes.shape) != 1:
-        raise RuntimeError(f"Use list, tuple or np.ndarray to pass multiple aruco sizes.")
-    if aruco_sizes.shape != (n,):
-        raise RuntimeError(
-            f"Number of aruco marker sizes does not correspond to "
-            f"the number of detected markers ({aruco_sizes.shape[0]} vs {n})")
+        if isinstance(aruco_sizes, (list, tuple)):
+            aruco_sizes = np.array(aruco_sizes)
+        elif not isinstance(aruco_sizes, np.ndarray):
+            aruco_sizes = np.array([aruco_sizes] * n)
+        if len(aruco_sizes.shape) != 1:
+            raise RuntimeError(f"Use list, tuple or np.ndarray to pass multiple aruco sizes.")
+        if aruco_sizes.shape != (n,):
+            raise RuntimeError(
+                f"Number of aruco marker sizes does not correspond to "
+                f"the number of detected markers ({aruco_sizes.shape[0]} vs {n})")
 
-    rvecs = list()
-    tvecs = list()
-    for i in range(n):
-        rvec, tvec, _ = \
-            cv2.aruco.estimatePoseSingleMarkers(corners[i], aruco_sizes[i], K, D)
-        rvecs.append(rvec)
-        tvecs.append(tvec)
-    rvecs = np.array(rvecs)
-    tvecs = np.array(tvecs)
-    # rvecs.shape = (n, 1, 3)
-    # tvecs.shape = (n, 1, 3)
+        rvecs = list()
+        tvecs = list()
+        for i in range(n):
+            rvec, tvec, _ = \
+                cv2.aruco.estimatePoseSingleMarkers(corners[i], aruco_sizes[i], K, D)
+            rvecs.append(rvec)
+            tvecs.append(tvec)
+        rvecs = np.array(rvecs)
+        tvecs = np.array(tvecs)
+        # rvecs.shape = (n, 1, 3)
+        # tvecs.shape = (n, 1, 3)
 
-    marker_poses = np.tile(np.eye(4), (n, 1, 1))
-    for i in range(n):
-        marker_poses[i, 0:3, 0:3], _ = cv2.Rodrigues(rvecs[i])
-        marker_poses[i, 0:3, 3] = tvecs[i, 0]
-    # marker_poses.shape = (n, 4, 4)
+        marker_poses = np.tile(np.eye(4), (n, 1, 1))
+        for i in range(n):
+            marker_poses[i, 0:3, 0:3], _ = cv2.Rodrigues(rvecs[i])
+            marker_poses[i, 0:3, 3] = tvecs[i, 0]
+        # marker_poses.shape = (n, 4, 4)
 
-    corners_3d_in_marker_frames = list()
-    for i in range(n):
-        corners_3d_in_single_marker_frame = list()
-        for sx, sy in [(-1, 1), (1, 1), (1, -1), (-1, -1)]:
-            # top left corner first
-            single_corner_3d_in_marker_frame = np.array(
-                [aruco_sizes[i] / 2 * sx,
-                 aruco_sizes[i] / 2 * sy,
-                 0, 1]).reshape(-1, 1)
-            corners_3d_in_single_marker_frame.append(single_corner_3d_in_marker_frame)
-            if not extract_all_corners:
-                break
-        corners_3d_in_single_marker_frame = np.array(corners_3d_in_single_marker_frame)
-        corners_3d_in_marker_frames.append(corners_3d_in_single_marker_frame)
-    corners_3d_in_marker_frames = np.array(corners_3d_in_marker_frames).swapaxes(0, 1)
-    # corners_3d_in_marker_frames.shape = (1 or 4, n, 4, 1)
+        corners_3d_in_marker_frames = list()
+        for i in range(n):
+            corners_3d_in_single_marker_frame = list()
+            for sx, sy in [(-1, 1), (1, 1), (1, -1), (-1, -1)]:
+                # top left corner first
+                single_corner_3d_in_marker_frame = np.array(
+                    [aruco_sizes[i] / 2 * sx,
+                    aruco_sizes[i] / 2 * sy,
+                    0, 1]).reshape(-1, 1)
+                corners_3d_in_single_marker_frame.append(single_corner_3d_in_marker_frame)
+                if not extract_all_corners:
+                    break
+            corners_3d_in_single_marker_frame = np.array(corners_3d_in_single_marker_frame)
+            corners_3d_in_marker_frames.append(corners_3d_in_single_marker_frame)
+        corners_3d_in_marker_frames = np.array(corners_3d_in_marker_frames).swapaxes(0, 1)
+        # corners_3d_in_marker_frames.shape = (1 or 4, n, 4, 1)
 
-    marker_corners_3d = np.matmul(marker_poses, corners_3d_in_marker_frames)
-    marker_corners_3d = marker_corners_3d[:, :, 0:3, 0].swapaxes(0, 1).reshape(-1, 3)
-    # marker_corners_3d.shape = (n or n * 4, 3)
+        marker_corners_3d = np.matmul(marker_poses, corners_3d_in_marker_frames)
+        marker_corners_3d = marker_corners_3d[:, :, 0:3, 0].swapaxes(0, 1).reshape(-1, 3)
+        # marker_corners_3d.shape = (n or n * 4, 3)
+    else:
+        marker_corners_3d = np.empty((0, 3))
+        rvecs = np.empty((0, 1, 3))
+        tvecs = np.empty((0, 1, 3))
 
     return marker_corners_3d, \
-        {'n': n, 'corners': corners, 'ids': ids, 'rvecs': rvecs, 'tvecs': tvecs}
+        {'n': n, 'corners': corners, 'ids': ids,
+        'rejected': rejected, 'rejected_n': rejected_n,
+        'rvecs': rvecs, 'tvecs': tvecs}
 
 
 def draw_aruco(image, corners, ids=None,
