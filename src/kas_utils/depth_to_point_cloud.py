ef5f235f7034f2223869829d97015095b8d308f6
andrey1908 <andrey.krishtopik@mail.ru>
2023-08-10 04:22:09 +0300

    depth_to_point_cloud.py: torch is not always faster than scipy; allow to choose backend; use scipy by default

--- src/kas_utils/depth_to_point_cloud.py
+++ src/kas_utils/depth_to_point_cloud.py
@@ -1,46 +1,49 @@
 import sys
 import numpy as np
+from scipy.ndimage import minimum_filter
 from .utils import get_depth_scale
 
-try:  # prefer using torch since it's faster
+try:
     import torch
 except ImportError:
-    from scipy.ndimage import minimum_filter
-    print(
-        "\033[1;93m"
-        "Could not import torch module. "
-        "Will use scipy.ndimage instead, but it's slower. "
-        "Recommend to install torch for better performance."
-        "\033[0m")
+    pass
 
 
 class DepthToPointCloud:
-    def __init__(self, K, D, pool_size=8, return_named_point_cloud=False):
+    def __init__(self, K, D, pool_size=8, return_named_point_cloud=False,
+            backend="scipy"):
+        assert backend in ["scipy", "torch"]
+
         self.K = K
         self.D = D
         self.pool_size = pool_size
         self.return_named_point_cloud = return_named_point_cloud
+        self.backend = backend
 
         self._check_D_is_zero()
 
-        if 'torch' in sys.modules:
-            self.pool_fn = self._pool_depth_with_torch
-        else:
+        if self.backend == "torch" and "torch" not in sys.modules:
+            raise RuntimeError(
+                "Requested backend is torch, "
+                "but torch module could not be imported.")
+        if self.backend == "scipy":
             self.pool_fn = self._pool_depth_with_scipy
+        else:
+            self.pool_fn = self._pool_depth_with_torch
 
     def _check_D_is_zero(self):
         assert np.all(self.D == 0), "D != 0, but only rectified depth is supported"
-
-    def _pool_depth_with_torch(self, depth):
-        depth = np.expand_dims(depth, axis=0)
-        max_pool_fn = torch.nn.MaxPool2d(self.pool_size)
-        pooled = -max_pool_fn(-torch.from_numpy(depth)).numpy().squeeze()
-        return pooled
 
     def _pool_depth_with_scipy(self, depth):
         pooled = minimum_filter(depth, size=self.pool_size,
             origin=-(self.pool_size // 2), mode='constant', cval=np.inf)
         pooled = pooled[::self.pool_size, ::self.pool_size]
+        return pooled
+
+    def _pool_depth_with_torch(self, depth):
+        depth = np.expand_dims(depth, axis=0)
+        max_pool_fn = torch.nn.MaxPool2d(self.pool_size)
+        pooled = -max_pool_fn(-torch.from_numpy(depth)).numpy().squeeze()
         return pooled
 
     def convert(self, depth):
