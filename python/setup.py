a175c4dc4aaf11b2ab01e26e1a6cce511aa18a13
andrey1908 <andrey.krishtopik@mail.ru>
2023-08-14 02:58:59 +0300

    python/setup.py: automatically update .so libraries in python/src/kas_utils folder

--- python/setup.py
+++ python/setup.py
@@ -1,9 +1,25 @@
 from setuptools import setup
+import glob
+import shutil
+import os
+import os.path as osp
+
+
+def update_libraries(source_path,
+        dst=osp.join(osp.dirname(__file__), "src/kas_utils/")):
+    for file in glob.glob(f"{dst}/py_*.so"):
+        os.remove(file)
+
+    for file in glob.glob(f"{source_path}/py_*.so"):
+        shutil.copy(file, dst)
+
+
+update_libraries(source_path=osp.join(osp.dirname(__file__), "../cpp/build/"))
 
 setup(
     name='kas_utils',
     version='0.0.1',
     packages=['kas_utils'],
     package_dir={'': 'src'},
-    package_data={'': ['py_depth_to_point_cloud.*.so']}
+    package_data={'': ['py_*.so']}
 )
